559312af4787ba8e3e0d56928c87eef768635547
j.faassen <j.faassen@linkorb.com>
2019-03-16 11:56:34 +0100

    Implement Ansible export

---
+++ src/Command/AnsibleExportInventoryCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Infra\Command;
+
+use RuntimeException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Infra\Infra;
+
+class AnsibleExportInventoryCommand extends AbstractCommand
+{
+    public function configure()
+    {
+        $this->setName('ansible:export-inventory')
+            ->setDescription('Export ansible inventory')
+        ;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $infra = $this->infra;
+        $data = [];
+        foreach ($infra->getResourcesByType('HostGroup') as $hostGroup) {
+            $data[$hostGroup->getName()] = [
+                'hosts' => [],
+                'children' => [],
+                'vars' => [],
+            ];
+            foreach ($hostGroup->getHosts() as $host) {
+                $data[$hostGroup->getName()]['hosts'][] = $host->getName();
+            }
+        }
+        $hostvars = [];
+        foreach ($infra->getResourcesByType('Host') as $host) {
+            $hostvars[$host->getName()] = [
+                'ansible_host' => $host->getPublicIp(),
+                'ansible_user' => $host->getSshUsername(),
+            ];
+        }
+        $data['_meta']['hostvars'] = $hostvars;
+        $output->writeLn(json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+    }
+}
