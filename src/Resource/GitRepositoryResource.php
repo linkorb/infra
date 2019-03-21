1b653668eee5825489f067606c1777204e93464d
Andrei Chugunov <admin@pythagor.com>
2019-03-21 23:05:50 +0400

    Add GitRepository Resource.

---
+++ src/Resource/GitRepositoryResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Infra\Resource;
+
+use GraphQL\Type\Definition\Type;
+use Infra\Infra;
+
+class GitRepositoryResource extends AbstractResource
+{
+    public function getUrl()
+    {
+        return $this->spec['url'] ?? null;
+    }
+
+    public function getRepositoryName()
+    {
+        $parsedUrl = $this->parseRepositoryUrl();
+
+        return $parsedUrl['name'];
+    }
+
+    public function getRepositoryOwnerName()
+    {
+        $parsedUrl = $this->parseRepositoryUrl();
+
+        return $parsedUrl['ownerName'];
+    }
+
+    public function getSshUrl()
+    {
+        $parsedUrl = $this->parseRepositoryUrl();
+
+        return 'git@' .
+            $parsedUrl['provider'] . ':' .
+            $parsedUrl['ownerName'] . '/' .
+            $parsedUrl['name'] . '.git';
+    }
+
+    public function getHttpUrl()
+    {
+        $parsedUrl = $this->parseRepositoryUrl();
+
+        return 'https://' .
+            $parsedUrl['provider'] . '/' .
+            $parsedUrl['ownerName'] . '/' .
+            $parsedUrl['name'] . '.git';
+    }
+
+    public function getViewUrl()
+    {
+        $parsedUrl = $this->parseRepositoryUrl();
+
+        return 'https://' .
+            $parsedUrl['provider'] . '/' .
+            $parsedUrl['ownerName'] . '/' .
+            $parsedUrl['name'];
+    }
+
+    public static function getConfig(Infra $infra)
+    {
+        return [
+            'name'   => 'GitRepository',
+            'fields' => [
+                'name'                => Type::id(),
+                'url'                 => Type::string(),
+                'description'         => Type::string(),
+                'repositoryName'      => Type::string(),
+                'repositoryOwnerName' => Type::string(),
+                'sshUrl'              => Type::string(),
+                'httpUrl'             => Type::string(),
+                'viewUrl'             => Type::string(),
+
+            ],
+        ];
+    }
+
+    private function parseRepositoryUrl()
+    {
+        $url = $this->getUrl();
+
+        preg_match('~^(.+)(@|\://)([^/\:@]+)(\:|/)([^/\.\:]+)/([^\./]+)\.git$~', $url, $matches);
+
+        if (7 !== count($matches)) {
+            throw new \RuntimeException("Can't parse repository url");
+        }
+
+        return [
+            'schema'    => $matches[1],
+            'provider'  => $matches[3],
+            'ownerName' => $matches[5],
+            'name'      => $matches[6],
+        ];
+    }
+}
